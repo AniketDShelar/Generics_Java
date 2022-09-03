d53ab5a3b08b87ee5f1319cb463126d157c62285
AniketDShelar <theaniketshelar@gmail.com>
2022-09-03 11:30:37 +0530

    Maximum String

--- src/com/bridgelabz/generics/Maximum.java
+++ src/com/bridgelabz/generics/Maximum.java
@@ -1,21 +1,20 @@
 package com.bridgelabz.generics;
 
 public class Maximum {
-    public static void main(String[] args) {
-        findMaxFloat(9.7f, 6.8f, 7.9f);
-    }
-
-    public static Float findMaxFloat(Float a, Float b, Float c) {
-        Float max = a;
+    public static String findMaxString(String a, String b, String c) {
+        String max = a;
         if (b.compareTo(max) > 0)
             max = b;
         if (c.compareTo(max) > 0)
             max = c;
 
-        System.out.println("First Number -> " + a);
-        System.out.println("Second Number -> " + b);
-        System.out.println("Third Number -> " + c);
-        System.out.println("Maximum Value is -> " + max);
+        System.out.println("First Number= " + a);
+        System.out.println("Second Number= " + b);
+        System.out.println("Thrird Number= " + c);
+        System.out.println("Maximum Value is= " + max);
         return max;
+    }
+    public static void main(String[] args) {
+        findMaxString("Mango", "orange", "Banana");
     }
 }
