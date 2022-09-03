d44106746d9c42eb7f5b9a4a9901d6c0c59edb02
AniketDShelar <theaniketshelar@gmail.com>
2022-09-03 11:28:39 +0530

    Maximum float

--- src/com/bridgelabz/generics/Maximum.java
+++ src/com/bridgelabz/generics/Maximum.java
@@ -1,8 +1,12 @@
 package com.bridgelabz.generics;
 
 public class Maximum {
-    public static Integer findMaxInteger(Integer a, Integer b, Integer c) {
-        Integer max = a;
+    public static void main(String[] args) {
+        findMaxFloat(9.7f, 6.8f, 7.9f);
+    }
+
+    public static Float findMaxFloat(Float a, Float b, Float c) {
+        Float max = a;
         if (b.compareTo(max) > 0)
             max = b;
         if (c.compareTo(max) > 0)
@@ -11,11 +15,7 @@ public class Maximum {
         System.out.println("First Number -> " + a);
         System.out.println("Second Number -> " + b);
         System.out.println("Third Number -> " + c);
-        System.out.println("Maximum Number -> " + max);
+        System.out.println("Maximum Value is -> " + max);
         return max;
-    }
-
-    public static void main(String[] args) {
-        Maximum.findMaxInteger(24, 19, 15);
     }
 }
